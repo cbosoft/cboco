44afa937406fcad397751feee06476032d0124b1
Christopher Boyle <chrismjboyle@gmail.com>
2023-07-14 18:46:41 +0100

    Add tests: loading and evaluate datasets from json

--- src/cboco/test_dataset_creation.py
+++ src/cboco/test_dataset_creation.py
@@ -1,3 +1,5 @@
+import os
+
 from cboco.dataset import Dataset, Category
 
 def test_dataset_creation_truly_empty():
@@ -9,3 +11,9 @@ def test_dataset_creation_truly_with_categories():
     assert len(dataset.categories) == 2
     assert dataset.categories[0].id == 1
     assert dataset.categories[1].id == 2
+
+def test_dataset_from_json():
+    dataset_a = Dataset.from_json(os.path.join('test_data', 'A.json'))
+    assert len(dataset_a.images) == 5
+    dataset_b = Dataset.from_json(os.path.join('test_data', 'B.json'))
+    assert len(dataset_b.images) == 5
---
+++ src/cboco/test_eval.py
@@ -0,0 +1,16 @@
+import os
+
+from cboco.dataset import Dataset
+from cboco.evaluation.evaluate_dataset import evaluate_dataset
+
+
+def test_eval():
+    true = Dataset.from_json(os.path.join('test_data', 'A.json'))
+    preds = Dataset.from_json(os.path.join('test_data', 'B.json'))
+    results = evaluate_dataset(
+        preds,
+        true,
+        iou_thresh=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+    )
+    assert abs(results['mF1'] - 0.625) < 1e-9
+    assert abs(results['mAP'] - 0.44619047619047614) < 1e-9
