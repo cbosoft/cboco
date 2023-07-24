0254b90a05bf13358a25093525f37d8b125e0a3d
Christopher Boyle <chrismjboyle@gmail.com>
2023-07-24 13:25:28 +0100

    Tidy up recalc func; name args properly

--- src/cboco/evaluation/precalculate.py
+++ src/cboco/evaluation/precalculate.py
@@ -6,14 +6,13 @@ from tqdm import tqdm
 from ..dataset import Annotation
 
 
-def precalculate_combinatorial_ious(a: List[Annotation], b: List[Annotation], method: Annotation.IoUMethod, show_progress: bool) -> Dict[Tuple[int, int], float]:
-    combinations = itertools.product(a, b)
-    n = len(a)*len(b)
-    it = combinations
+def precalculate_combinatorial_ious(tann: List[Annotation], pann: List[Annotation], method: Annotation.IoUMethod, show_progress: bool) -> Dict[Tuple[int, int], float]:
+    combinations = itertools.product(tann, pann)
+    n = len(tann)*len(pann)
     if show_progress:
-        it = tqdm(it, total=n)
+        combinations = tqdm(combinations, total=n)
     ious = {}
-    for t, p in it:
+    for t, p in combinations:
         if t.image_id == p.image_id:
             ious[t.id, p.id] = t.iou(p, method=method)
     return ious
